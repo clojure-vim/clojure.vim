ff77d5e72ed7d44a1fa0fd1fa04943457f39676e
Juho Teperi <juho.teperi@iki.fi>
2020-12-30 18:52:04 +0200

    Remove private field access from generate ns
    Use Unicode standard files for regex categories, blocks and scripts,
    and just hardcode POSIX, Java and binary regex properties.

--- clj/project.clj
+++ clj/project.clj
@@ -4,5 +4,6 @@
   :license {:name "Vim License"
             :url "http://vimdoc.sourceforge.net/htmldoc/uganda.html#license"
             :comments ":help license"}
-  :dependencies [[org.clojure/clojure "1.10.1"]
+  :dependencies [[org.clojure/data.csv "1.0.0"]
+                 [org.clojure/clojure "1.10.1"]
                  [frak "0.1.9"]])
