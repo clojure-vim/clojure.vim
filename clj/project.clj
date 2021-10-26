6c70003142874f266af814aa526650f6b2e474c8
Alex Vear <alex@vear.uk>
2021-10-26 16:30:10 +0100

    Regenerate for latest Clojure and Java versions
    (+ updated other dependencies.)

--- clj/project.clj
+++ clj/project.clj
@@ -5,5 +5,5 @@
             :url "http://vimdoc.sourceforge.net/htmldoc/uganda.html#license"
             :comments ":help license"}
   :dependencies [[org.clojure/data.csv "1.0.0"]
-                 [org.clojure/clojure "1.10.2"]
+                 [org.clojure/clojure "1.10.3"]
                  [frak "0.1.9"]])
