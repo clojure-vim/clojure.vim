aee8c8dbd7d4dc570e02c837dc04925e9b478643
Alex Vear <alex@vear.uk>
2022-08-08 20:09:55 +0100

    Remove global `test-case-dir` var

--- clj/test/vim/indent_test.clj
+++ clj/test/vim/indent_test.clj
@@ -5,8 +5,6 @@
             [vim.helpers :as h])
   (:import [java.io File]))
 
-(def ^:private test-case-dir (io/file (io/resource "indent-test-cases")))
-
 (defn get-test-cases [^File test-case-dir]
   (into []
         (comp
@@ -14,7 +12,7 @@
           (map #(.getName ^File %)))
         (.listFiles test-case-dir)))
 
-(defn run-test-case [test-case]
+(defn run-test-case [test-case-dir test-case]
   (testing (str "Preparation for " test-case)
     (let [input    (io/file test-case-dir test-case "in.clj")
           expected (io/file test-case-dir test-case "out.clj")
@@ -36,8 +34,10 @@
 ;; TODO: do this parallisation more intelligently with agents.
 (deftest test-indent
   "Runs all indentation tests in parallel"
-  (doseq [{:keys [test-case expected expected-file actual actual-file]}
-          (pmap run-test-case (get-test-cases test-case-dir))]
-    (testing test-case
-      (is (= expected actual)
-          (format "(not= \"%s\"\n      \"%s\")" expected-file actual-file)))))
+  (let [test-case-dir (io/file (io/resource "indent-test-cases"))
+        test-cases    (get-test-cases test-case-dir)]
+    (doseq [{:keys [test-case expected expected-file actual actual-file]}
+            (pmap (partial run-test-case test-case-dir) test-cases)]
+      (testing test-case
+        (is (= expected actual)
+            (format "(not= \"%s\"\n      \"%s\")" expected-file actual-file))))))
