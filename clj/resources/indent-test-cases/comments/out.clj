4edeef035ff90b97fecba46593b28c5344740dba
Alex Vear <alex@vear.uk>
2023-12-23 19:22:28 +0000

    Add tests to check that comments don't affect indentation

---
+++ clj/resources/indent-test-cases/comments/out.clj
@@ -0,0 +1,11 @@
+{:foo {:bar 1}                    ; Default.
+ :biz {:as 123, :asdf #{1 2 3}}
+ }
+
+;; Foo bar! {
+{:foo {:bar 1}     ; Default. {
+ :biz {:as 123, :asdf #{1 2 3}}}
+
+{:foo {:bar 1}     ; Default.
+ ;; Foo bar! {
+ :biz {:as 123, :asdf #{1 2 3}}}
