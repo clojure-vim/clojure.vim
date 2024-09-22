6a9f98ec3fa0e29b174d7b74012daa0de6ac37b0
Alex Vear <alex@vear.uk>
2024-09-22 22:35:54 +0100

    Update for Clojure 1.12.0

--- clj/project.clj
+++ clj/project.clj
@@ -4,6 +4,6 @@
   :license      {:name     "Vim License"
                  :url      "http://vimdoc.sourceforge.net/htmldoc/uganda.html#license"
                  :comments ":help license"}
-  :dependencies [[org.clojure/clojure "1.11.1"]
-                 [org.clojure/data.csv "1.0.1"]
+  :dependencies [[org.clojure/clojure "1.12.0"]
+                 [org.clojure/data.csv "1.1.0"]
                  [frak "0.1.9"]])
